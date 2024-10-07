479af5d04ef581ca7b3358d89fd1a28bf293e757
Donovan Dall <d@nuff.tech>
2024-10-07 11:49:54 +0100

    fix

--- config/languages/solidity.nix
+++ config/languages/solidity.nix
@@ -1,6 +1,18 @@
 {
   extraConfigLua = ''
-    require'lspconfig'.solidity_ls_nomicfoundation.setup{}
+    local lspconfig = require 'lspconfig'
+    local configs = require 'lspconfig.configs'
+
+    configs.solidity = {
+      default_config = {
+        cmd = {'nomicfoundation-solidity-language-server', '--stdio'},
+        filetypes = { 'solidity' },
+        root_dir = lspconfig.util.find_git_ancestor,
+        single_file_support = true,
+      },
+    }
+
+    lspconfig.solidity.setup {}
+    -- require'lspconfig'.solidity_ls_nomicfoundation.setup{}
   '';
-  
 }
